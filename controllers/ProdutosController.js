d3a658891997de4bd35f27899f01fe7b032fe10a
DSM2 <fatec-dsm2@academico.intra>
2024-04-18 19:18:12 -0300

    criacao do banco com campos usando sequelize

--- controllers/ProdutosController.js
+++ controllers/ProdutosController.js
@@ -1,16 +1,25 @@
 import express from 'express';
+import Produto from '../models/Produto.js';
 const router = express.Router();
 
+// router.get("/produtos", (req, res) => {
+//     const produtos = [
+//         { nome: "Celular Motorola E22", preco: 1200, categoria: "Eletroportáteis" },
+//         { nome: "Tablet Samsung", preco: 900, categoria: "Eletrônicos" },
+//         { nome: "Notebook Lenovo", preco: 3200, categoria: "Computadores" },
+//         { nome: "Fone Bluetooth", preco: 150, categoria: "Periféricos" }
+//     ];
+//     res.render("produtos", {
+//         produtos: produtos
+//     });
+// });
+
 router.get("/produtos", (req, res) => {
-    const produtos = [
-        { nome: "Celular Motorola E22", preco: 1200, categoria: "Eletroportáteis" },
-        { nome: "Tablet Samsung", preco: 900, categoria: "Eletrônicos" },
-        { nome: "Notebook Lenovo", preco: 3200, categoria: "Computadores" },
-        { nome: "Fone Bluetooth", preco: 150, categoria: "Periféricos" }
-    ];
-    res.render("produtos", {
-        produtos: produtos
-    });
-});
+    Produto.findAll().then(produtos => {
+        res.render("produtos", {
+            produtos: produtos
+        })
+    })
+})
 
 export default router;
